f308c5b151542aeaee1d6d1b331886850cc5628c
Andolaus <andolaus@gmail.com>
2025-09-02 08:17:05 +0200

    Finished oppgave 0-5

--- intro-javascript/oppgave_0/src/losning.js
+++ intro-javascript/oppgave_0/src/losning.js
@@ -25,8 +25,37 @@ changeButton.addEventListener('click', () => {
 // Selecting input field, button and text element by their IDs
 const inputField = document.getElementById('input');
 const inputText = document.getElementById('input-text');
-
+// Adding event listener to input field to update text element as user types
 inputField.addEventListener('input', () => {
     inputText.textContent = inputField.value;
+});
+
+// Oppgave 4
+// Selecting button and text element by their IDs
+const writeListButton = document.getElementById('write-list');
+const ul = document.getElementById('ul');
+
+// List of items to be added to the unordered list
+writeListButton.addEventListener('click', () => {
+    for (item of myList) {
+        const li = document.createElement('li');
+        li.textContent = item;
+        ul.append(li);
+    }
+});
+
+// Oppgave 5
+// Selecting select element, text input, button and placeholder div by their IDs
+const selectElement = document.getElementById('select');
+const textContent = document.getElementById('text');
+const createButton = document.getElementById('create');
+const placeholder = document.getElementById('placeholder');
+
+// Adding event listener to button to create new element based on selected value and input text
+createButton.addEventListener('click', () => {
+    const selectedValue = selectElement.value;
+    const newElement = document.createElement(selectedValue);
+    newElement.textContent = textContent.value;
+    placeholder.append(newElement);
 });
 
